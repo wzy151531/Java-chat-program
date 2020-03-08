a5b037ac07b6815248bda9c5027a83bfa745882f
Ziyan Wang <zxw989@cs.bham.ac.uk>
2020-03-08 17:37:48 +0000

    delete some comments

--- socotra-server/src/main/java/socotra/service/ServerThread.java
+++ socotra-server/src/main/java/socotra/service/ServerThread.java
@@ -82,36 +82,6 @@ public class ServerThread extends Thread {
                     default:
                         System.out.println("Unknown data.");
                 }
-//                if (connectionData.getType() == 0) {
-////                    username = connectionData.getUsername();
-////                    String password = connectionData.getPassword();
-////                    try {
-////                        if (!JdbcUtil.validateUser(username, password)) {
-////                            System.out.println("Invalidated user.");
-////                            toClient.writeObject(new ConnectionData(false));
-////                            endClient();
-////                            return;
-////                        } else {
-////                            Server.addClient(username, toClient);
-////                            System.out.println("Validated user. Current online users: " + Server.getClients().keySet());
-////                            toClient.writeObject(new ConnectionData(true));
-////                            TreeSet<String> allClientsName = new TreeSet<>(Server.getClients().keySet());
-////                            allClientsName.remove(username);
-////                            // Inform the new client current online users and inform other clients that the new client is online.
-////                            Server.broadcast(new ConnectionData(username, true), username, new ConnectionData(allClientsName));
-////                        }
-////                    } catch (Exception e) {
-////                        e.printStackTrace();
-////                    }
-//                } else if (connectionData.getType() == -2) {
-////                    Server.broadcast(new ConnectionData(username, false), username);
-////                    Server.removeClient(username);
-////                    System.out.println("User log out. Current online users: " + Server.getClients().keySet());
-////                    endClient();
-////                    return;
-//                } else {
-////                    Server.broadcast(connectionData);
-//                }
             }
         } catch (IOException e) {
             System.out.println("Something went wrong. Ending service to client...");
